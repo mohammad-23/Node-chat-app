df8cb2869def0b9dace9c8467897bb0f070ffba2
Mohammad <mohammad8758@hotmail.com>
2018-04-28 09:26:31 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.port || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+
+
+
+app.listen(3000, () => {
+    console.log(`Server up on port ${port}`);
+});
+
+
+
+
+
