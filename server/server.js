c036729dc26481fe4ee8ff4b1dccf45b1762c7f8
Mohammad <mohammad8758@hotmail.com>
2018-05-01 23:01:21 +0530

    chalgo shaayad

--- server/server.js
+++ server/server.js
@@ -9,14 +9,13 @@ const {Users} = require('./utils/users');
 
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 var users = new Users();
 
 app.use(express.static(publicPath));
-
 
 io.on('connection', (socket) => {
     console.log('New user connected');
